73dd2edcddb632fd4e0e26e193a51a3ce3cb9c5c
Abdullokh Alimov <66109159+Alimov-8@users.noreply.github.com>
2020-11-21 23:07:03 +0500

    Update algo_vis.java

--- algo_vis.java
+++ algo_vis.java
@@ -1,1 +1,19 @@
-Hello World !
+public class Algorithm_Visualizer {
+    public static void main(String[] args){
+        Window W = new Window();
+    }
+}
+
+class Window extends JFrame {//inheriting JFrame
+    JFrame f;
+
+    Window() {
+        JButton b = new JButton("click");//create button
+        b.setBounds(130, 100, 100, 40);
+
+        add(b);//adding button on frame
+        setSize(400, 500);
+        setLayout(null);
+        setVisible(true);
+    }
+}
