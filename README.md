1a3ebe7daf0f50fa600ec8887858ef402de4574f
Rustam-Z <zokirovrustam202@gmail.com>
2020-12-13 11:27:47 +0500

    action listeners added to Sorting.java buttons

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Updated: December 5, 2020
 - https://www.geeksforgeeks.org/java-swing-jmenubar/
 
 - https://stackoverflow.com/questions/31094904/jmenu-not-appearing-until-window-is-resized
+
+- https://stackoverflow.com/questions/4716372/java-how-do-i-close-a-jframe-while-opening-another-one
--- Sorting.java
+++ Sorting.java
@@ -11,7 +11,7 @@ public class Sorting extends Main {
     JPanel pPanel1, pPanel2;
 
     // Sorting Buttons
-    JButton jbtRandomize, jbtReset, jbtBubble, jbtInsertion, jbtSelection, jbtStart;  // Sorting Buttons
+    JButton jbtRandomize, jbtMerge, jbtBubble, jbtInsertion, jbtSelection, jbtStart;  // Sorting Buttons
 
     // Random Creator
     Random rand = new Random();
@@ -23,18 +23,18 @@ public class Sorting extends Main {
         // Create Panel
         // Panel for options (bubble sort, insertion sort...)
         pPanel1 = new JPanel();        
-        pPanel1.setLayout(new GridLayout(6,1));
+        pPanel1.setLayout(new GridLayout(1, 7));
 
         // Panel for main algorithm
         pPanel2 = new JPanel();
-        pPanel2.setLayout(new GridLayout(10,1));
+        pPanel2.setLayout(new GridLayout(10, 1));
 
         // Buttons for sorting
         jbtRandomize = new JButton("Randomize");//create button
-        jbtReset = new JButton("Reset");//create button
-        jbtBubble = new JButton("Bubble sort");//create button
-        jbtInsertion = new JButton("Insertion sort");//create button
-        jbtSelection = new JButton("Selection sort");//create button
+        jbtMerge = new JButton("Merge Sort");//create button
+        jbtBubble = new JButton("Bubble Sort");//create button
+        jbtInsertion = new JButton("Insertion Sort");//create button
+        jbtSelection = new JButton("Selection Sort");//create button
         jbtStart = new JButton("Start");//create button
         jbtStart.setBackground(Color.GREEN);
         
@@ -44,7 +44,7 @@ public class Sorting extends Main {
         jb1.setStringPainted(true);
 
         // Adding elements to Panel 1
-        pPanel1.add(jbtRandomize); pPanel1.add(jbtReset); pPanel1.add(jbtSelection);
+        pPanel1.add(jbtRandomize); pPanel1.add(jbtMerge); pPanel1.add(jbtSelection);
         pPanel1.add(jbtBubble); pPanel1.add(jbtInsertion); pPanel1.add(jbtStart);
 
         // Adding elements to Panel 2
@@ -54,9 +54,34 @@ public class Sorting extends Main {
         pPanel1.setBackground(Color.CYAN);
         pPanel2.setBackground(Color.YELLOW);
 
+        // Register listeners
+        ListenerClass listener = new ListenerClass();
+        jbtRandomize.addActionListener(listener);
+        jbtMerge.addActionListener(listener);
+        jbtBubble.addActionListener(listener);
+        jbtInsertion.addActionListener(listener);
+        jbtSelection.addActionListener(listener);
+        jbtStart.addActionListener(listener);
+
         // Add Panels to the panel
         add(pPanel1, BorderLayout.NORTH);
         add(pPanel2, BorderLayout.CENTER);
-
     }
+
+    class ListenerClass implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == jbtRandomize)
+                System.out.println("jbtRandomize button clicked");
+            else if (e.getSource() == jbtMerge)
+                System.out.println("jbtMerge button clicked");
+            else if (e.getSource() == jbtBubble)
+                System.out.println("jbtBubble button clicked");
+            else if (e.getSource() == jbtInsertion)
+                System.out.println("jbtInsertion button clicked");
+            else if (e.getSource() == jbtSelection)
+                System.out.println("jbtSelection button clicked");
+            else if (e.getSource() == jbtStart)
+                System.out.println("jbtStart button clicked");
+        }
+    } 
 }
